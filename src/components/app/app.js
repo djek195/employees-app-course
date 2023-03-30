863a605f1b932e65d17d844248bfd69d885bfd94
djek195 <djek195@gmail.com>
2023-03-30 23:16:18 +0300

    studied componenst and how to create react app firstly

---
+++ src/components/app/app.js
@@ -0,0 +1,43 @@
+import "./app.css";
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+function App() {
+  const data = [
+    {
+      name: "Анастасія Кравченко",
+      salary: 800,
+      increase: false,
+      id: 1,
+    },
+    {
+      name: "Євгеній Калініченко",
+      salary: 3000,
+      increase: true,
+      id: 2,
+    },
+    {
+      name: "Віталій Танюк",
+      salary: 5000,
+      increase: false,
+      id: 3,
+    },
+  ];
+  return (
+    <div className="app">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
+}
+
+export default App;
